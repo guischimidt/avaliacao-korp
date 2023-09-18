df3024d199908aea39c56d81619beab7e67bf2a6
Guilherme Schimidt <guigoschimidt@gmail.com>
2023-09-18 14:42:42 -0300

    feat: create api service

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import {
+    HttpClient,
+    HttpErrorResponse,
+    HttpHeaders,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Person } from '../models/person';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ApiService {
+    url = 'http://localhost:3000/';
+
+    constructor(private httpClient: HttpClient) {}
+
+    httpOptions = {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    };
+
+    savePerson(person: Person): Observable<Person> {
+        return this.httpClient
+            .post<Person>(`${this.url}user`, person, this.httpOptions)
+            .pipe(catchError(this.handleError));
+    }
+
+    getPersons(): Observable<any> {
+        return this.httpClient
+            .get<any>(`${this.url}user`)
+            .pipe(catchError(this.handleError));
+    }
+
+    saveAccount(values: any): Observable<any> {
+        return this.httpClient
+            .post<any>(
+                `${this.url}account/${values.userId}`,
+                values,
+                this.httpOptions
+            )
+            .pipe(catchError(this.handleError));
+    }
+
+    handleError(error: HttpErrorResponse) {
+        let errorMessage = '';
+        errorMessage = error.error.error;
+        return throwError(() => new Error(errorMessage));
+    }
+}
