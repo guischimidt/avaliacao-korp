4392acffd31752051e59360a115332f8d9c46f89
Guilherme Schimidt <guigoschimidt@gmail.com>
2023-09-18 21:59:37 -0300

    feat: add deposit and withdraw options

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { ApiService } from '../services/api.service';
+import { ResetFormService } from '../services/reset-form.service';
 
 @Component({
     selector: 'app-transactions',
     templateUrl: './transactions.component.html',
-    styleUrls: ['./transactions.component.sass']
+    styleUrls: ['./transactions.component.sass'],
 })
-export class TransactionsComponent {
+export class TransactionsComponent implements OnInit {
+    transactionForm: FormGroup;
+    users: any;
+    accounts: any;
 
+    constructor(
+        private fb: FormBuilder,
+        private apiService: ApiService,
+        private resetFormService: ResetFormService
+    ) {
+        this.transactionForm = this.fb.group({
+            userId: [null],
+            accountId: [null],
+            amount: [null],
+            transactionType: [null],
+        });
+    }
+
+    ngOnInit(): void {
+        this.apiService.getPersons().subscribe((res) => {
+            this.users = res;
+        });
+
+        this.transactionForm.get('userId')?.valueChanges.subscribe((userId) => {
+            if (userId) {
+                this.apiService.getAccounts().subscribe((res) => {
+                    this.accounts = res;
+                    this.accounts = this.accounts.filter(
+                        (account: { userId: { _id: any } }) =>
+                            account.userId._id === userId
+                    );
+                });
+            }
+        });
+    }
+
+    message = '';
+    type = '';
+
+    onSubmit() {
+        if (this.transactionForm.valid) {
+            this.apiService
+                .saveTransaction(this.transactionForm.value)
+                .subscribe({
+                    next: () => {
+                        this.message = 'Transação realizada com sucesso';
+                        this.type = 'success';
+                        this.resetForm();
+                        this.ngOnInit();
+                    },
+                    error: (error) => {
+                        this.message = error.message;
+                        this.type = 'error';
+                    },
+                });
+        } else {
+            console.log('Formulário inválido. Não pode ser enviado.');
+        }
+    }
+
+    resetForm() {
+        this.resetFormService.resetForm(this.transactionForm);
+    }
 }
--- src/app/transactions/transactions.module.ts
+++ src/app/transactions/transactions.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 
 import { TransactionsComponent } from './transactions.component';
+import { MessagesModule } from '../shared/messages/messages.module';
 
 @NgModule({
     declarations: [TransactionsComponent],
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatButtonModule,
+        MatTableModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        NgxMaskDirective,
+        NgxMaskPipe,
+        MessagesModule,
+    ],
+    providers: [provideNgxMask()],
 })
 export class TransactionsModule {}
