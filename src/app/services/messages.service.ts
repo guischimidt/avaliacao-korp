1e86e3f11007052719e04885e0ac2343db3e48bb
Guilherme Schimidt <guigoschimidt@gmail.com>
2023-09-27 19:04:15 -0300

    refactor: change setTimeout to timer from rxjs

--- src/app/services/messages.service.ts
+++ src/app/services/messages.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Subject, Observable } from 'rxjs';
+import { Subject, Observable, timer } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root',
 })
 export class MessagesService {
     private messagesSubject = new Subject<{ message: string; type: string }>();
+    private messageTimeout = 6000;
 
     sendMessage(message: string, type: string) {
         this.messagesSubject.next({ message, type });
 
-        setTimeout(() => {
-            this.clearMessage();
-        }, 6000);
+        timer(this.messageTimeout)
+            .pipe(take(1))
+            .subscribe(() => {
+                this.clearMessage();
+            });
     }
     getMessage(): Observable<{ message: string; type: string }> {
         return this.messagesSubject.asObservable();
