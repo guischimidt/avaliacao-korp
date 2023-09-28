bb6ede41a51ef25ce0d5dda5367ed0ff70e86a92
Guilherme Schimidt <guigoschimidt@gmail.com>
2023-09-28 17:52:16 -0300

    refactor: persons is default page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/contas', pathMatch: 'full' },
+    { path: '', redirectTo: '/pessoas', pathMatch: 'full' },
 
     //Lazy load com rotas filhas
     {
